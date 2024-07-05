14fa498c3e354d35c626eccd0e232237fb2899b3
USER1995 <userfrom1995@gmail.com>
2024-07-05 22:19:58 +0530

    FirstCommit

---
+++ Employee.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+class Employee {
+    public:
+     string Name;
+     string Company;
+
+
+};
+int main() {
+Employee e;
+
+e.Company = "XYZ";
+e.Name = "John";
+
+
+return 0;
+}
